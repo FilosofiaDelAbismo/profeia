9ad4f73db229eb6e67c4e1e3b66cfe6a375a6c16
FilosofiaDelAbismo <danielpajarobueno@gmail.com>
2026-04-06 15:47:30 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.options = {
     "domain": "3nbf4.com",
-    "zoneId": 10839907
+    "zoneId": 10840139
 }
 self.lary = ""
 importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
